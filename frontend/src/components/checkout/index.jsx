bca737088d2ca3a368e4f92722c3d3009ba9ac61
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-24 12:17:00 +0000

    refactor: main checkout component Removed unused imports and got rid of commented out components

--- frontend/src/components/checkout/index.jsx
+++ frontend/src/components/checkout/index.jsx
@@ -4,10 +4,8 @@ import "./checkout.css";
 import Cart from "./cart";
 
 import { useStateValue } from "../../StateProvider";
-import CheckoutProduct from "./product";
 
 function Checkout() {
-  const [{ basket }] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,18 +13,8 @@ function Checkout() {
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423493668_.jpg"
         />
-        <div>
-          <Cart />
-          {/* {basket.map((item) => (
-            <CheckoutProduct
-              key={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))} */}
-        </div>
+
+        <Cart />
       </div>
       <div className="checkout__right">{/* <Subtotal /> */}</div>
     </div>
