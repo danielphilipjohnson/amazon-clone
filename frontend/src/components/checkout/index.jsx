ad10537598c4793fc6341222f88329cea5db7d9e
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-24 09:55:14 +0000

    styling: added svg to style empty carts and made amazon clone buttons

--- frontend/src/components/checkout/index.jsx
+++ frontend/src/components/checkout/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./checkout.css";
+import checkoutImage from "../../images/checkout/shopping.svg";
 import Subtotal from "../subtotal";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "./product";
@@ -17,6 +18,30 @@ function Checkout() {
         <div>
           <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Shopping Basket</h2>
+
+          {user || (
+            <div className="empty-cart">
+              <div className="left">
+                <a href="https://www.freevector.com/grocery-shopping-girl-illustration-vector-29602">
+                  <img
+                    className="empty-cart__image"
+                    src={checkoutImage}
+                    alt=""
+                  />
+                </a>
+              </div>
+
+              <div className="empty-cart__right">
+                <h2>Your shopping basket is empty</h2>
+                <a href="/">Shop today's deals</a>
+
+                <button className="btn-amazon btn-amazon-active">
+                  Sign in to your account
+                </button>
+                <button className="btn-amazon">Sign up now</button>
+              </div>
+            </div>
+          )}
           {basket.map((item) => (
             <CheckoutProduct
               key={item.id}
