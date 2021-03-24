d239bf0a6dbe0a090bb061014a2b2007a43e3174
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-24 11:23:46 +0000

    added prime image and basic product layout

--- frontend/src/components/checkout/product/index.jsx
+++ frontend/src/components/checkout/product/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import FadeIn from "react-fade-in";
+import PrimeLogo from "../../../images/prime-logo.png";
 import "./product.css";
 import { useStateValue } from "../../../StateProvider";
-import FadeIn from "react-fade-in";
 
 function CheckoutProduct({ id, image, title, price, rating, hideButton }, ref) {
   const [{ basket }, dispatch] = useStateValue();
@@ -19,11 +20,17 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }, ref) {
       <div className="checkoutProduct" key={id}>
         <img className="checkoutProduct__image" src={image} />
         <div className="checkoutProduct__info">
-          <p className="checkoutProduct__title">{title}</p>
-          <p className="checkoutProduct__price">
-            <small>$</small>
-            <strong>{price}</strong>
-          </p>
+          <div className="checkoutProduct__top">
+            <p className="checkoutProduct__title alink-normal">{title}</p>
+            <p className="checkoutProduct__price">
+              <small>$</small>
+              <strong>{price}</strong>
+            </p>
+          </div>
+          <p>In stock</p>
+          <div className="i-con">
+            <img src={PrimeLogo} alt="" />
+          </div>
           <div className="checkoutProduct__rating">
             {Array(rating)
               .fill()
@@ -32,7 +39,31 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }, ref) {
               ))}
           </div>
           {!hideButton && (
-            <button onClick={removeFromBasket}>Remove from Basket</button>
+            <>
+              <span className="seperator">| </span>
+              <button onClick={removeFromBasket} className="a-color-link">
+                Delete
+              </button>
+              <span className="seperator">| </span>
+              <button
+                onClick={() => {
+                  console.log("to be implemented");
+                }}
+                className="a-color-link"
+              >
+                Save for later
+              </button>
+              <span className="seperator">| </span>
+              <button
+                onClick={() => {
+                  console.log("to be implemented");
+                }}
+                className="a-color-link"
+              >
+                See more like this
+              </button>
+              <span className="seperator">| </span>
+            </>
           )}
         </div>
       </div>
