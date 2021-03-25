bb3d184040702244ef374e87a4bd3d5346761610
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-25 15:48:00 +0000

    features: built item delivery

--- frontend/src/components/payment/index.jsx
+++ frontend/src/components/payment/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./payment.css";
 import AmazonLogo from "../../images/amazon-logo-black.png";
 import { useStateValue } from "../../StateProvider";
+import PrimeLogo from "../../images/prime-logo.png";
+
 import CheckoutProduct from "../checkout/product";
 import { Link, useHistory } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
@@ -121,58 +123,126 @@ function Payment() {
       <div className="payment__container">
         <div className="payment-card">
           {/* Payment section - delivery address */}
-          <div className="payment__section">
-            <ol className="payment__details">
-              <li className="payment__details-list">
-                <div className="payment__section">
-                  <div className="payment__title">
-                    <h3>Delivery Address</h3>
+          <div>
+            <div className="payment__section">
+              <ol className="payment__details">
+                <li className="payment__details-list">
+                  <div className="payment__section">
+                    <div className="payment__title">
+                      <h3>Delivery Address</h3>
+                    </div>
+                    <div className="payment__address">
+                      <p>{user?.email}</p>
+                      <p>123 React Lane</p>
+                      <p>Los Angeles, CA</p>
+                    </div>
                   </div>
-                  <div className="payment__address">
-                    <p>{user?.email}</p>
-                    <p>123 React Lane</p>
-                    <p>Los Angeles, CA</p>
-                  </div>
-                </div>
-              </li>
-              <li className="payment__details-list">
-                {/* Payment section - Payment method */}
-                <div className="payment__section">
-                  <div className="payment__title">
-                    <h3>Payment Method</h3>
-                  </div>
-                  <div className="payment__details">
-                    {/* Stripe magic will go */}
+                </li>
+                <li className="payment__details-list">
+                  {/* Payment section - Payment method */}
+                  <div className="payment__section">
+                    <div className="payment__title">
+                      <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                      {/* Stripe magic will go */}
 
-                    <form onSubmit={handleSubmit}>
-                      <CardElement onChange={handleChange} />
+                      <form onSubmit={handleSubmit}>
+                        <CardElement onChange={handleChange} />
 
-                      {/* Errors */}
-                      {error && <div>{error}</div>}
-                    </form>
+                        {/* Errors */}
+                        {error && <div>{error}</div>}
+                      </form>
+                    </div>
                   </div>
-                </div>
-              </li>
-              <li className="payment__details-list">
-                {/* Payment section - Review Items */}
-                <div className="payment__section">
-                  <div className="payment__title">
-                    <h3>Review items and delivery</h3>
+                </li>
+                <li className="payment__details-list">
+                  {/* Payment section - Review Items */}
+                  <div className="payment__section">
+                    <div className="payment__title">
+                      <h3>Review items and delivery</h3>
+                    </div>
                   </div>
                   <div className="payment__items">
+                    <h2 className="color-success">
+                      Guaranteed delivery: 28 Mar. 2021
+                    </h2>{" "}
+                    <span>
+                      If you order in the next 6 hours and 9 minutes. Details
+                    </span>
                     {basket.map((item) => (
-                      <CheckoutProduct
-                        id={item.id}
-                        title={item.title}
-                        image={item.image}
-                        price={item.price}
-                        rating={item.rating}
-                      />
+                      <div className="reviewProduct" key={item.id}>
+                        <img
+                          className="reviewProduct__image"
+                          src={item.image}
+                        />
+                        <div className="reviewProduct__info">
+                          <p className="reviewProduct__title">{item.title}</p>
+                          <div className="reviewProduct__price">
+                            <p className="color-price">
+                              <small>$</small>
+                              {item.price}{" "}
+                            </p>
+                            <img src={PrimeLogo} alt="" />
+                          </div>
+                          <span className="color-mute">
+                            Sold by:Amazon EU S.a.r.L.
+                          </span>
+                        </div>
+
+                        <div>
+                          <p className="reviewForm__heading">
+                            Choose your Prime delivery option:
+                          </p>
+                          <form action="">
+                            <div className="form-group">
+                              <input
+                                type="radio"
+                                id="male"
+                                name="gender"
+                                value="male"
+                              />
+                              <label for="male" className="form__delivery">
+                                FREE One-Day Delivery :{" "}
+                                <span className="color-success">
+                                  get it Tomorrow, Mar. 26
+                                </span>
+                              </label>
+                            </div>
+
+                            <br />
+                            <div className="form-group">
+                              <input
+                                type="radio"
+                                id="male"
+                                name="gender"
+                                value="male"
+                              />
+                              <label for="male" className="form__delivery">
+                                FREE Amazon Day Delivery :
+                                <span className="color-success">
+                                  get it Sunday, Mar. 28
+                                </span>
+                              </label>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
                     ))}
                   </div>
-                </div>
-              </li>
-            </ol>
+                </li>
+              </ol>
+            </div>
+            <div className="buynow__bottom">
+              <button className="btn-amazon btn-amazon-active">Buy now</button>
+
+              <h3>Order Total: $1.95</h3>
+              <p>
+                By placing your order you agree to Amazon's Conditions of Use &
+                Sale. Please see our Privacy Notice, our Cookies Notice and our
+                Interest-Based Ads Notice.
+              </p>
+            </div>
           </div>
 
           {/* <div className="payment__priceContainer">
