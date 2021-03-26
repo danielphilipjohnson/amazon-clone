dca3d77e21fda4256a8b3db139307bf130dbbdb8
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-26 12:22:08 +0000

    style: created the about product section

--- frontend/src/components/product/index.js
+++ frontend/src/components/product/index.js
@@ -45,42 +45,71 @@ function Product({ id }) {
           </div>
 
           <div className="product-description__main__extra">
-            <p>
+            <p className="product-description__main__extra-text">
               May be available at a lower price from{" "}
               <span className="alink-normal inline">other sellers</span>,
               potentially without free Prime shipping.{" "}
             </p>
-            <p>
+            <p className="product-description__main__extra-text">
               <b>Note:</b> This item is eligible for{" "}
               <b>FREE click and collect</b> without a minimum order.
               <span className="alink-normal inline"> Details</span>
             </p>
-            <p>
-              {" "}
+            <p className="product-description__main__extra-text">
               <span className="alink-normal inline">New (5) </span>from
               <span className="color-total">£10.03 </span>{" "}
               <b>& FREE Delivery</b>
             </p>
+
+            <ul className="reset-list">
+              <li className="flex between">
+                <b>Brand</b>
+                <p>Funko</p>
+              </li>
+              <li className="flex between">
+                <b>Material</b>
+                <p>Vinyl</p>
+              </li>
+              <li className="flex between">
+                <b>Size</b>
+                <p>6.4 x 6.4 x 9.5 centimetres</p>
+              </li>
+              <li className="flex between">
+                <b> Item weight </b>
+                <p>0.3 Pounds</p>
+              </li>
+            </ul>
           </div>
 
-          <ul>
-            <li>
-              <p>Brand</p>
-              <p>Funko</p>
-            </li>
-            <li>
-              <p>Material</p>
-              <p>Vinyl</p>
-            </li>
-            <li>
-              <p>Size</p>
-              <p>6.4 x 6.4 x 9.5 centimetres</p>
-            </li>
-            <li>
-              <p> Item weight </p>
-              <p>0.3 Pounds</p>
-            </li>
-          </ul>
+          <div className="product-description__about">
+            <h2 className="product-description__about-heading">
+              About this item
+            </h2>
+            <ul className="product-description__about-list reset-list">
+              <li className="">
+                <p>
+                  From Dragon Ball Z, Vegetal, as a stylized POP vinyl from
+                  Funko
+                </p>
+              </li>
+              <li className="">
+                <p>
+                  Stylized collectable stands 3 ¾ inches tall, perfect for any
+                  Dragon Ball Z fan{" "}
+                </p>
+              </li>
+              <li className="">
+                <p>Collect and display all Dragon Ball Z POP Vinyl's </p>
+              </li>
+              <li className="">
+                <p>
+                  Funko POP is the 2018 Toy of the Year and People's Choice
+                  award winner{" "}
+                </p>
+              </li>
+            </ul>
+            <p className="alink-normal">See more product details</p>
+          </div>
         </div>
       </div>
       <div className="product-form">
