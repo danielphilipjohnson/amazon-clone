6782e851b72863aa1ae095a97aae52a53401d1b0
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-26 09:48:32 +0000

    cart styling refactored

--- frontend/src/components/checkout/cart/your-items/index.js
+++ frontend/src/components/checkout/cart/your-items/index.js
@@ -12,7 +12,7 @@ function YourItems() {
       <div className="customer-items__header">
         <h2 className="customer-items__heading">Your items</h2>
         <div className="btn-group">
-          <button className="btn-tab btn-tab-active">
+          <button className="btn-tab btn-tab--active">
             Saved for later (26 items){" "}
           </button>
           <button className="btn-tab">Buy it again </button>
