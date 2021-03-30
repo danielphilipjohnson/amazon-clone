aa1c59fcf6c5a0c0d0af98c905cde0ef5d33085a
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-30 13:46:12 +0100

    added a loading effect

--- frontend/src/routes/products/list-products/index.js
+++ frontend/src/routes/products/list-products/index.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import Product from "./product";
 import data from "../../../adapters/productAdapter";
 
+import "./loading.css";
+
 function ListProducts() {
   const [product, setProduct] = useState(null);
 
   const fetchMatchedProducts = async () => {
-    await setProduct(data);
-    // setProduct(null);
+    // await setProduct(data);
+    setProduct(null);
   };
 
   useEffect(() => {
@@ -19,7 +21,19 @@ function ListProducts() {
       return <Product productInformation={item} />;
     });
   } else {
-    return <h1>loading</h1>;
+    return (
+      <div class="wrapper">
+        <div class="wrapper-cell">
+          <div class="image animated-background"></div>
+          <div class="text">
+            <div class="text-line animated-background"></div>
+            <div class="text-line animated-background"></div>
+            <div class="text-line animated-background"></div>
+            <div class="text-line animated-background"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
