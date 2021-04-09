03182d103f0ba394bd12c9b7bfc7322c0d19beb2
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-04-09 13:53:29 +0100

    browsing history fetches items from backend

--- frontend/src/components/shared/product-list/item/index.jsx
+++ frontend/src/components/shared/product-list/item/index.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import generateStars from "./utils";
-
-function Item({ id, image, description, rating, price, likes }) {
+import "./index.css";
+import PrimeLogo from "../../../../images/prime-logo.png";
+function Item({ id, title, image, description, rating, price, likes }) {
+  const formatImagePathFromBackend = (imageUrl) => {
+    return `${process.env.REACT_APP_IMAGEHOST}${imageUrl} `;
+  };
   return (
-    <div className="browsering__items-product">
-      <img src={image} alt="product description" />
+    <div className="browsering__items-product" key={id}>
+      <img
+        className="browsering__items-image"
+        src={formatImagePathFromBackend(image.url)}
+        alt="product description"
+      />
       <div className="browsering__items-description-text">
-        <p>{description}</p>
+        <p>{title}</p>
         <p>
           <span className="rating">{generateStars(rating)}</span>{" "}
           <span>{likes}</span>
         </p>
+        <div className="prime__delivery">
+          <img
+            className="product-description__primelogo-item"
+            src={PrimeLogo}
+          />
+          <span>FREE One-Day</span>
+        </div>
+
         <p className="price">{price}</p>
       </div>
     </div>
